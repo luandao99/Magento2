e4e122084c9ccf3a7bb1f0c5cd8205c0f20f13c2
luan <luandaoit@gmail.com>
2023-02-16 11:32:47 +0700

    et/c

--- app/code/Local/Blog/registration.php
+++ app/code/Local/Blog/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'VendorName_ModuleName',
+    'Local_Blog',
     __DIR__
 );
