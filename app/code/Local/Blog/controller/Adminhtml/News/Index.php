268577a1ed97e568f95ff31d05d8d70b3e2a3f5f
luan <luandaoit@gmail.com>
2023-02-20 21:56:41 +0700

    Created Adminhtml>>>>Menu news Admin

---
+++ app/code/Local/Blog/controller/Adminhtml/News/Index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Local\Blog\Controller\Adminhtml\News;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory = false;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend((__('News')));
+
+        return $resultPage;
+    }
+}
