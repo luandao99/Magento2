19badcbb9767b99a1913c2938473c489a1312e9d
luan <luandaoit@gmail.com>
2023-02-21 10:16:24 +0700

    exex

--- app/code/Local/Blog/Model/Blog.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Local\Blog\Model;
-class Blog extends \Magento\Framework\Model\AbstractModel
-{
-    protected function _construct()
-    {
-        $this->_init('Local\Blog\Model\ResourceModel\Blog');
-    }
-}
